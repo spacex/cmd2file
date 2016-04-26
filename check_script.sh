4596f62a983be5e72f91c4b1dc7b225f76fa71a0
Michael Williams <mspacex@gmail.com>
2016-04-25 22:15:06 -0500

    Make check script more versatile.

--- check_script.sh
+++ check_script.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-while true; do if [ -e /tmp/fifo ]; then echo -n "Checking ... "; md5sum /tmp/fifo; fi; sleep .01; done
+SKIP=0
+while true; do
+  if [ -e /tmp/fifo ]; then
+    if [ ! $SKIP -eq 0 ]; then
+      echo
+    fi
+    SKIP=0
+    echo -n "Checking ... "
+    md5sum /tmp/fifo
+    [[ "$1" != "" ]] && sleep $1
+  else
+    if [ $SKIP -eq 0 ]; then
+      echo -n "Skipping ."
+    else
+      echo -n "."
+    fi
+    SKIP=$(($SKIP+1))
+  fi
+done
